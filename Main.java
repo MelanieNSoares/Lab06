5b479459a70a2232fe79de0e90d4b28d3eee990d
gloomel <41337940+gloomel@users.noreply.github.com>
2020-04-21 18:45:20 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,39 @@
 
+public class Main{
+  public static void main(String args[]){
+
+    Tabuleiro tabuleiro = new Tabuleiro();
+
+    //Preencher a tabela
+    for(int i = 0; i<= 8; i++){ 
+      for(int j= 0; j<= 8; j++){
+
+        if((i<4) && ((i % 2 != 0) && (j % 2 != 0)){
+          Tabuleiro.createTable(new Comum(1),i,j);
+        }
+        else if ((i<4) && ((i % 2 == 0) && (j % 2 == 0)){
+          Tabuleiro.createTable(new Comum(1),i,j);
+        }
+
+        else if(( (i<9 && i>6) && ((i % 2 != 0) && (j % 2 != 0)){
+          Tabuleiro.createTable(new Comum(0),i,j);
+        }
+        else if ((i<9 && i>6) && ((i % 2 == 0) && (j % 2 == 0)){
+          Tabuleiro.createTable(new Comum(0),i,j);
+        }
+
+        else if(i>3 && i<6){
+          Tabuleiro.createTable(null,i,j);
+
+        }
+      
+      }
+    }
+
+
+
+
+
+
+  }
+}
