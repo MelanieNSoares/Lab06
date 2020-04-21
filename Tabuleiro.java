446c3dc9ec221a38f427684385b2015ee503fdea
gloomel <41337940+gloomel@users.noreply.github.com>
2020-04-21 20:07:48 -0300

    Update Tabuleiro.java

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -1,8 +1,20 @@
 public class Tabuleiro{
-  Peca vTabuleiro[8][8];
+  public Peca vTabuleiro[][] = new Peca[8][8];
 
   public void createTable(Peca peca, int linha, int coluna){
     vTabuleiro[linha][coluna] = peca;
+  }
+
+  public void capture(int foward, int diagonal, int i, int j, int fator){
+    vTabuleiro[i + foward][j+diagonal] = null;
+    move(foward,diagonal,i,j,fator);
+   
+  }
+
+  public void move(int foward, int diagonal, int i, int j,int fator){
+    Peca pecaMovendo = vTabuleiro[i][j];
+    vTabuleiro[i][j] = null;
+    vTabuleiro[i + (fator * foward)][j+ (fator * diagonal)] = pecaMovendo;
   }
   
 }
